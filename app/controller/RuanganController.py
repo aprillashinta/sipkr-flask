ae54374ac9a4fabe53626b9602b96e5df2cae813
Nantaaa <anantah738@gmail.com>
2026-01-05 22:12:31 +0700

    controller: implement crud logic for ruangan
    Signed-off-by: Nantaaa <anantah738@gmail.com>

---
+++ app/controller/RuanganController.py
@@ -0,0 +1,23 @@
+from app.model.ruangan import Ruangan
+from app import db, response
+from flask import request
+
+def index():
+    try:
+        ruangan = Ruangan.query.all()
+        data = [{'id': r.id, 'nama': r.nama_ruang, 'kapasitas': r.kapasitas} for r in ruangan]
+        return response.success(data, "Berhasil mengambil data ruangan")
+    except Exception as e:
+        return response.error([], str(e))
+
+def store():
+    try:
+        nama = request.form.get('nama_ruang')
+        kapasitas = request.form.get('kapasitas')
+        
+        baru = Ruangan(nama_ruang=nama, kapasitas=kapasitas)
+        db.session.add(baru)
+        db.session.commit()
+        return response.success('', 'Ruangan berhasil ditambahkan!')
+    except Exception as e:
+        return response.error([], str(e))
